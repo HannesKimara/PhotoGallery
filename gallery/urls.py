4ac6ec935f12670fff8ada4485db45fa003fecef
Hannes Kimara <hanneskimara@gmail.com>
2020-03-02 01:08:22 +0300

    Implement Search functionality
    Create search.html
    
    Create search url route
    
    Create search_results view in views module

--- gallery/urls.py
+++ gallery/urls.py
@@ -7,6 +7,7 @@ app_name = 'gallery'
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('search', views.search_results, name = 'search_results')
 ]
 
 if settings.DEBUG:
--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Image
 
 def index(request):
     all_images = Image.objects.all()
     return render(request, 'index.html', {'images': all_images})
+
+def search_results(request):
+    if 'q' in request.GET and request.GET["q"]:
+        query_item = request.GET.get("q")
+        related_images = Image.search_image(query_item)
+    else:
+        return render(request, 'search.html', {'images': []})
+
+    return render(request, 'search.html', {'images': related_images})
