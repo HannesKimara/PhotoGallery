4850a21a82572d8aaa8b4e9e09ae2c1d40605a89
Hannes Kimara <hanneskimara@gmail.com>
2020-03-02 00:02:28 +0300

    Create modal to show on image click

--- gallery/static/js/main.js
+++ gallery/static/js/main.js
@@ -4,3 +4,14 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Sidenav.init(elems, options);
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    var options = {};
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, options);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var options = {};
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems, options);
+  });
