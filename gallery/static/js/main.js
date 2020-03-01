7c7c7e7926c017c2289964db8ea5a72c737e8275
Hannes Kimara <hanneskimara@gmail.com>
2020-03-02 02:54:34 +0300

    Implement link copy and toast notification

--- gallery/static/js/main.js
+++ gallery/static/js/main.js
@@ -14,4 +14,19 @@ document.addEventListener('DOMContentLoaded', function() {
     var options = {};
     var elems = document.querySelectorAll('.materialboxed');
     var instances = M.Materialbox.init(elems, options);
-  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var options = {};
+    var elems = document.querySelectorAll('.tooltipped');
+    var instances = M.Tooltip.init(elems, options);
+});
+
+function clipboardCopy(text) {
+    navigator.clipboard.writeText(text).then(
+        function(){
+            M.toast({html: `Copied ${text} to clipboard`, 
+                    displayLength: 2500,
+            })
+        })
+}
